c4d104fa2ff84b61730e9a81638dc865cefce25a
an.chugaeva <chugaeva96nastya@gmail.com>
2023-12-18 14:17:34 +0200

    fixed mistakes

--- homework03.py
+++ homework03.py
@@ -1,4 +1,4 @@
-"""03 homework."""
+"""Re-write text with using format () and f-string ."""
 norway_text = ('Automatisering akselererer %syeblikket da roboter vil erobre '
                'planeten v%sr. (%s)' % ('ø', 'å', 'Æ'))
 print(norway_text)
@@ -7,6 +7,6 @@ norway_text1 = ('Automatisering akselererer {0}yeblikket da roboter vil '
                 'erobre planeten v{1}r. ({2})'.format('ø', 'å', 'Æ'))
 print(norway_text1)
 # rewrate with f-string
-norway_text2 = f'Automatisering akselererer {'ø'}yeblikket da roboter  vil '
-f'erobre planeten v{'å'}r. ({'Æ'})'
+norway_text2 = (f'Automatisering akselererer {"ø"}yeblikket da roboter  vil '
+                f'erobre planeten v{"å"}r. ({"Æ"})')
 print(norway_text2)
